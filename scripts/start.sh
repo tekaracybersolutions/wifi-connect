46f2fc33440646bd00777070fd29c4c06eafbb1f
Moses3301 <moseshaina@gmail.com>
2024-08-15 10:19:14 +0300

    fix the port

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # sometimes. In this case, following checks will fail and wifi-connect
 # will be launched even if the device will be able to connect to a WiFi network.
 # If this is your case, you can wait for a while and then check for the connection.
-# sleep 15
+#sleep 3
 
 # Choose a condition for running WiFi Connect according to your use case:
 
@@ -20,15 +20,16 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # wget --spider http://google.com 2>&1
 
 # 4. Is there an active WiFi connection?
-#iwgetid -r
+iwgetid -r
 
-ping -c 1 8.8.8.8 >/dev/null 2>&1
+#ping -c 4 8.8.8.8 >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
 else
     printf 'Starting WiFi Connect\n'
-    ./wifi-connect
+
+    ./wifi-connect > wificonnect.log
 fi
 
 # Start your application here.
